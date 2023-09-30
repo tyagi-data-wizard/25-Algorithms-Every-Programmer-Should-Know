9d371f8f402354e364947d9a4cb9952d72592f9f
Shrutideshpande2163 <146514326+Shrutideshpande2163@users.noreply.github.com>
2023-10-01 00:26:16 +0530

    DFS_shruti.java

--- 1 . Searching/DFS.java
+++ 1 . Searching/DFS.java
@@ -17,6 +17,45 @@ class Graph {
     public void addEdge(int v, int w) {
         adj[v].add(w);
     }
+    /* 
+    Graph Class:
+
+This class represents a graph using an adjacency list.
+It has private instance variables:
+V: The number of vertices in the graph.
+adj: An array of linked lists, where each element in the array represents a vertex, and the linked list stores its adjacent vertices.
+Constructor:
+
+The Graph constructor takes an integer v as a parameter, representing the number of vertices in the graph.
+It initializes the V variable and creates an array of linked lists (adj) with a length of v. Each linked list represents the adjacency list for a vertex.
+addEdge Method:
+
+This method allows you to add an edge between two vertices in the graph.
+It takes two integer parameters v and w, indicating that there is a directed edge from vertex v to vertex w.
+It adds vertex w to the adjacency list of vertex v.
+DFSUtil Method:
+
+This is a private recursive helper method for performing Depth-First Search (DFS) starting from a specific vertex.
+It takes two parameters: an integer v (the current vertex) and a boolean array visited that keeps track of visited vertices.
+It marks the current vertex as visited by setting visited[v] to true and prints the vertex value.
+Then, it recursively explores all unvisited neighbors of the current vertex by calling DFSUtil on them.
+DFS Method:
+
+This method allows you to perform DFS traversal starting from a specific vertex.
+It takes one parameter, an integer v, which is the starting vertex for DFS traversal.
+It initializes a boolean array visited with false values to keep track of visited vertices.
+It calls the DFSUtil method to start the DFS traversal from vertex v.
+main Method:
+
+The main method is the entry point of the program.
+It creates an instance of the Graph class with 7 vertices.
+It adds edges to the graph to create a simple directed graph with multiple branches.
+Finally, it calls the DFS method starting from vertex 0 and prints the result of the DFS traversal.
+Overall Functionality:
+
+This code demonstrates how to create a graph and perform Depth-First Search (DFS) traversal starting from a specified vertex.
+The DFS algorithm explores vertices and their neighbors in a depth-first manner, printing the visited vertices in the order they are encountered.
+    */
 
     // Recursive DFS function starting from vertex v
     private void DFSUtil(int v, boolean[] visited) {
