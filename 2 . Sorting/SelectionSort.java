ec768e0f8989d1482ddb03098da3deb0ad65e214
Apurva Pawar <102655654+ApurvaPawar05@users.noreply.github.com>
2023-10-01 00:11:53 +0530

    Update SelectionSort.java

--- 2 . Sorting/SelectionSort.java
+++ 2 . Sorting/SelectionSort.java
@@ -40,3 +40,17 @@ public class SelectionSort {
         System.out.println();
     }
 }
+
+/* This Java program implements the Selection Sort algorithm to sort an array of integers in ascending order. Here's a short description of what the program does:
+
+1. The `selectionSort` method takes an integer array as input and sorts it using the Selection Sort algorithm.
+   - It iterates through the array, considering each element as the potential minimum.
+   - For each element, it searches the rest of the unsorted portion of the array to find the minimum element.
+   - Once the minimum element is found, it swaps it with the element at the current iteration's position.
+   - This process continues until the entire array is sorted.
+
+2. The `main` method initializes an integer array with unsorted values, prints the original array, then calls `selectionSort` to sort the array, and finally prints the sorted array.
+
+3. The `printArray` method is a utility function used to print the contents of an array.
+
+In summary, this program demonstrates the Selection Sort algorithm by sorting an array of integers and displaying both the original and sorted arrays. */
