1899381824c7d97d8d1d27d9d2e7912ca58ec127
Mohit Wat <116029995+mohitwat7@users.noreply.github.com>
2023-10-01 00:24:28 +0530

    Update Euclidean Algorithm 002.java

--- 5 . Basic/Euclidean Algorithm 002.java
+++ 5 . Basic/Euclidean Algorithm 002.java
@@ -13,3 +13,7 @@ public class EuclideanAlgorithm {
         System.out.println("GCD of " + a + " and " + b + " is " + gcdResult);
     }
 }
+
+/*This Java program implements the Euclidean Algorithm to find the greatest common divisor (GCD) of two integers. 
+    The `gcd` method is a recursive function that efficiently calculates the GCD by repeatedly taking the remainder of the division until the remainder is zero. 
+    The `main` method demonstrates the usage by finding and printing the GCD of two predefined integers, 48 and 18.*/
